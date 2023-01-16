b8b0a10008fc848d5d797882fbeb40069f602dcf
gitcontrolsetings <misicratko.sicmi@gmail.com>
2023-01-16 18:53:46 +0100

    Dodan Seminar

---
+++ Expert.RatkoMisic.Seminar/Controllers/SeminarController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using System.Collections.Specialized;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Expert.RatkoMisic.Seminar.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SeminarController : ControllerBase
+
+
+
+{
+    [HttpGet()]
+    public int Get()
+    { return 5; }
+
+            
+
+   
+
+}
+    
+  
+   
+  
+
+
+   
+   
+       
+
+    
+
+    
+    
+
+    
+
+    
+
+    
+
+    
+   
+    
+
+    
+   
+    
+        
+            
+        
+    
+
+
+    
+    
+
+       
+       
+
+    
+
+
