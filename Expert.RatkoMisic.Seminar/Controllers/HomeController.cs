ccc3553d912634465dedcf6942c3cf4edd147fd5
gitcontrolsetings <misicratko.sicmi@gmail.com>
2023-01-17 09:25:32 +0100

    RatkoMišić

---
+++ Expert.RatkoMisic.Seminar/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Expert.RatkoMisic.Seminar.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
